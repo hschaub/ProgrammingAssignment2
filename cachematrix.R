9c0f938a054e18ebf2f92b1190b5543a04b153fb
gabrielangelos <heik1@htomail.de>
2019-07-01 21:49:02 +0200

    Commit for the programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates an inverted matrix and stores it in the cache 
+## cahceSolve retrieves the cached inverted matrix according to the 
+## makeCacheMatrix function 
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix function firstly sets the value of the matrix according 
+## to x and clears any previously stored inverted matrix in im.
+## Then the setter and the getter are defined in setinv and getinv.
+## Finally a list is created that allows to access these operators in later
+## functions.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        im <- NULL
+        set <- function(y) {
+                x <<- y
+                im <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) im <<- solve
+        getinv <- function() im
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve retrives the cached inverted matrix as set by
+## the makeCacheMatrix function.
+## If no inverted matrix has been found in x the cacheSolve 
+## function sets the inverted matrix via the setinv function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        im <- x$getinv()
+        if(!is.null(im)) {
+                message("getting cached data")
+                return(im)
+        }
+        data <- x$get()
+        im <- solve(data, ...)
+        x$setinv(im)
+        im
 }
